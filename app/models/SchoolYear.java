e168dbac46c5c184738dfd48e9800c926c176c81
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-05-16 21:16:11 -0400

    Update SchoolYear.java

--- app/models/SchoolYear.java
+++ app/models/SchoolYear.java
@@ -2,11 +2,19 @@ package models;
 
 import java.time.LocalDateTime;
 
-
+/**
+* a helper class that contains methods that return the specified school year
+* @author victor portia grace jaron farbod
+* 
+**/
 public class SchoolYear {
   static LocalDateTime date;  
   
-      
+    /**
+    * a method that returns the LocalDateTime of the start of the school year
+    * @return schoolYear
+    *            LocalDateTime of the start of schoolyear
+    **/
     public static LocalDateTime currentSchoolYear() {
       LocalDateTime schoolYear = LocalDateTime.of(LocalDateTime.now().getYear(), 9, 1, 0, 0);
       
@@ -17,7 +25,12 @@ public class SchoolYear {
       return schoolYear;
     }
     
-  
+    /**
+    * A method that returns the LocalDateTime of the year given a string of the first year
+    * @param year
+    *          the string of the first year
+    * @return LocalDateTime of the start of school year
+    **/
     public static LocalDateTime toSchoolYear(String year) {
       int firstYear = Integer.parseInt(year.substring(0,3));
       return LocalDateTime.of(firstYear, 9, 1, 0, 0);
